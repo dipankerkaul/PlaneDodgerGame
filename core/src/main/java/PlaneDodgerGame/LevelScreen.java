6d27f715b92427e1a699ced56c258c91620b4195
Jane Doe <42276172+dipankerkaul@users.noreply.github.com>
2020-07-24 19:49:53 +0530

    Third testable commit!

--- core/src/main/java/PlaneDodgerGame/LevelScreen.java
+++ core/src/main/java/PlaneDodgerGame/LevelScreen.java
@@ -1,10 +1,15 @@
 package PlaneDodgerGame;
 
 import com.badlogic.gdx.Input.Keys;
-
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.math.MathUtils;
 
 public class LevelScreen extends BaseScreen
 {   Plane plane;
+    float starTimer;
+    float starSpawnInterval;
+    int score;
+    Label scoreLabel;
 
     public void initialize() 
     {   new Sky(0,0, mainStage);
@@ -13,12 +18,35 @@ public class LevelScreen extends BaseScreen
         new Ground(800,0, mainStage);
         plane = new Plane(100, 500, mainStage);
         BaseActor.setWorldBounds(800,600);
+        starTimer = 0;
+        starSpawnInterval = 4;
+        score = 0;
+        scoreLabel = new Label( Integer.toString(score), BaseGame.labelStyle );
+        uiTable.pad(10);
+        uiTable.add(scoreLabel);
+        uiTable.row();
+        uiTable.add().expandY();
 
 
     }
 
     public void update(float dt)
     {
+        starTimer += dt;
+        if (starTimer > starSpawnInterval)
+        {
+            new Star( 800, MathUtils.random(100,500), mainStage );
+            starTimer = 0;
+        }
+        for (BaseActor star : BaseActor.getList(mainStage, Star.class))
+        {
+            if (plane.overlaps(star))
+            {
+                star.remove();
+                score++;
+                scoreLabel.setText( Integer.toString(score) );
+            }
+        }
        
     }
 
